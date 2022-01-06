5908159630d791b372d59bc0823a3f94489c2d8a
rumor1993 <rumor1993@naver.com>
2022-01-07 00:14:35 +0900

    case2 구분자를 기준으로 분리한 각 숫자의 합을 반환 [완료]

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,11 +3,7 @@ import java.util.List;
 public class Calculator {
     public int add(List<Integer> numbers) {
         int result = 0;
-
-        for (Integer item : numbers) {
-            result =+ item;
-        }
-
+        for (Integer item : numbers) result += item;
         return result;
     }
 }
